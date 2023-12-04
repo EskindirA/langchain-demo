83d9adda386049d396eb102b8d5c230cf4b8791a
Eskindir Abebe Geda <eskindir-abebe.geda@broadcom.com>
2023-12-04 20:20:08 +0100

    updated settings

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 
 try{
     const result = await fetch('java_summary.txt');
     const text = await result.text();
+
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize:500
+    });
+    const output = splitter.createDocuments([text]);
+    
+    console.log(output);
 }catch(err){
     console.log(err);
 }
